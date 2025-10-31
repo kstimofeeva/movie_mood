876b392427eeb20c0c3a958f642d57da9eb68efe
kstimofeeva <ke_timofeeva@mail.ru>
2025-10-31 13:41:59 +0300

    add classes Movie and Review

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # movie_mood
+1. PostgreSQL схема
+база данных состоит из двух таблиц (movies, reviews)
+movies содержит атрибуты: id, title, genre, description, mood, created_at
+reviews содержит атрибуты: id, movie_id, review_text, sentiment, sentiment_score, created_at
+2. 
--- database/models.py
+++ database/models.py
@@ -1,1 +1,28 @@
+from sqlalchemy import create_engine, Column, Integer, String, Text, Float, DateTime, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, relationship
+from datetime import datetime
+
+class Base(DeclarativeBase): pass
+
+class Movie(Base):
+    __tablename__ = 'movies'
+    id = Column(Integer, primary_key = True, index = True)
+    title = Column(String(255), index = True)
+    genre = Column(String(100))
+    description = Column(Text)
+    mood = Column(String(50))
+    created_at = Column(DateTime, default = datetime.utcnow)
+
+    reviews = relationship('Review', back_populates = 'reviews')
+
+class Review(Base):
+    __tablename__ = 'reviews'
+    id = Column(Integer, primary_key = True, index = True)
+    movie_id = Column(Integer, ForeignKey('movies.id', ondalete = 'CASCADE'))
+    review_text = Column(Text)
+    sentiment = Column(String(50))
+    sentiment_score = Column(Float)
+    created_at = Column(DateTime, default = datetime.utcnow)
+
+    movies = relationship('Movie', back_populates = 'movies')
 
