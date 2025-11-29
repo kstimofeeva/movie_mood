d72272b3554d8f38118295595f7ec6c73f18d747
kstimofeeva <ke_timofeeva@mail.ru>
2025-11-29 22:33:21 +0300

    add review's schemas

--- schemas/review_schemas.py
+++ schemas/review_schemas.py
@@ -1,1 +1,17 @@
+from pydantic import BaseModel, ConfigDict
+from datetime import datetime
+from typing import Optional
 
+class ReviewBase(BaseModel):
+    review_text: str
+
+class ReviewCreate(ReviewBase):
+    movie_id: int
+
+class Review(ReviewBase):
+    model_config = ConfigDict(from_attributes = True)
+    id: int
+    movie_id : int
+    sentiment: Optional[str] = None
+    sentiment_score: Optional[float] = None
+    created_at: datetime
