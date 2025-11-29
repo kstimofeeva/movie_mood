66edf0ec48432c34f5f5ef72b7ab448f01ea8960
kstimofeeva <ke_timofeeva@mail.ru>
2025-11-29 23:53:58 +0300

    add functions for movie_service and try-except

--- services/movie_service.py
+++ services/movie_service.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 from database.models import Movie
 from schemas.movie_schemas import MovieCreate, MovieUpdate
 
@@ -8,36 +9,69 @@ class MovieService:
 
 #добавление фильма в бд
     def create_movie(self, movie_data: MovieCreate) -> Movie:
-        db_movie = Movie (
-            title = movie_data.title,
-            genre = movie_data.genre,
-            description = movie_data.description
-        )
+        try:
+            db_movie = Movie (
+                title = movie_data.title,
+                genre = movie_data.genre,
+                description = movie_data.description
+            )
 
-        self.db.add(db_movie)
-        self.db.commit()
-        self.db.refresh(db_movie)
-        return db_movie
+            self.db.add(db_movie)
+            self.db.commit()
+            self.db.refresh(db_movie)
+            return db_movie
+        except IntegrityError:
+            self.db.rollback()
+            raise ValueError("Фильм с таким названием уже существует или нарушена целостность данных")
+        except SQLAlchemyError:
+            self.db.rollback()
+            raise Exception("Ошибка при сохранении фильмов в БД")
+        except Exception:
+            self.db.rollback()
+            raise Exception("Ошибка сервера")
 
     #получение фильма по айдишнику
     def get_movie(self, movie_id: int) -> Movie:
-        return self.db.query(Movie).filter(Movie.id == movie_id).first()
+        try:
+            movie = self.db.query(Movie).filter(Movie.id == movie_id).first()
+            return movie
+        except SQLAlchemyError:
+            self.db.rollback()
+            raise Exception("Ошибка при получении данных")
+        except Exception:
+            self.db.rollback()
+            raise Exception("Ошибка сервера")
+
 
     #поиск фильма по названию / жанру
     def search_movies(self, title: str = None, genre: str = None):
-        query = self.db.query(Movie)
-        if title and genre:
-            query = query.filter(Movie.title.ilike(f"%{title}%") and Movie.genre.ilike(f"{genre}"))
-        elif title:
-            query = query.filter(Movie.title.ilike(f"%{title}%"))
-        elif genre:
-            query = query.filter(Movie.genre.ilike(f"%{genre}%"))
-        return query.all()
-
+        try:
+            query = self.db.query(Movie)
+            if title and genre:
+                query = query.filter(Movie.title.ilike(f"%{title}%") and Movie.genre.ilike(f"{genre}"))
+            elif title:
+                query = query.filter(Movie.title.ilike(f"%{title}%"))
+            elif genre:
+                query = query.filter(Movie.genre.ilike(f"%{genre}%"))
+            return query.all()
+        except SQLAlchemyError:
+            self.db.rollback()
+            raise Exception("Ошибка при поиске фильмов в БД")
+        except Exception:
+            self.db.rollback()
+            raise Exception("Ошибка сервера")
     #фильм по настроению
     def get_movies_by_mood(self, mood: str):
-        return self.db.query(Movie).filter(Movie.mood.ilike(f"%{mood}%")).all()
-
+        try:
+            if not mood:
+                raise ValueError("Не указано настроение для фильма")
+            return self.db.query(Movie).filter(Movie.mood.ilike(f"%{mood}%")).all()
+        except SQLAlchemyError:
+            self.db.rollback()
+            raise Exception("Ошибка при поиске фильмов по настроению")
+        except Exception:
+            self.db.rollback()
+            raise Exception("Ошибка сервера")
     #обновление инфы о фильме
     def update_movie(self, movie_id: int, movie_data: MovieUpdate):
         try:
@@ -60,3 +94,18 @@ class MovieService:
         except Exception:
             self.db.rollback()
             raise Exception
+
+    def delete_movie(self, movie_id: int) -> bool:
+        try:
+            movie = self.get_movie(movie_id)
+            if not movie:
+                return False
+            self.db.delete(movie)
+            self.db.commit()
+            return True
+        except SQLAlchemyError:
+            self.db.rollback()
+            raise Exception("Ошибка при удалении фильма из БД")
+        except Exception:
+            self.db.rollback()
+            raise Exception("Ошибка сервера")
