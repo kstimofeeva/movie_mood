d17860f40bb4b97b51cc7a718648256dc81e5611
kstimofeeva <ke_timofeeva@mail.ru>
2025-11-29 22:29:18 +0300

    add movie's schemas

--- schemas/movie_schemas.py
+++ schemas/movie_schemas.py
@@ -1,1 +1,28 @@
+from pydantic import BaseModel, ConfigDict
+from typing import Optional
+from datetime import datetime
+
+class MovieBase(BaseModel):
+    title: str
+    genre: Optional[str] = None
+    description: Optional[str] = None
+
+#добавление фильма
+class MovieCreate(MovieBase):
+    pass
+
+#обновление инфы про фильм
+class MovieUpdate(BaseModel):
+    title: Optional[str] = None
+    genre: Optional[str] = None
+    description: Optional[str] = None
+    mood: Optional[str] = None
+
+#возвращает данные пользователю
+class Movie(MovieBase):
+    model_config = ConfigDict(from_attributes = True)
+    id: int
+    mood: Optional[str] = None
+    created_at: datetime
+
 
