aaca726dd6254f7a26efbaa3581a77dc24d02591
kstimofeeva <ke_timofeeva@mail.ru>
2025-12-20 21:12:55 +0300

    add __init__ for schemas and services

---
+++ schemas/__init__.py
@@ -0,0 +1,4 @@
+from .movie_schemas import MovieBase, MovieCreate, MovieUpdate, Movie
+from .review_schemas import ReviewBase, ReviewCreate, Review
+
+__all__ = ['MovieBase', 'MovieCreate', 'MovieUpdate', 'Movie', 'ReviewBase', 'ReviewCreate', 'Review']
